ee6afe9701b4dfa860c101877809b0dbfd4f7256
Ernest <ernestwinata@gmail.com>
2023-06-11 22:49:40 -0700

    creates myfile.md

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
+  const { id, prompt, answers } = question;
+  const [correctIdx, setCorrectIdx] = useState(question.correctIndex);
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +10,48 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+    function handleDelete() {
+      fetch(`http://localhost:4000/questions/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then(() => {
+          onDeleteQuestion((questions) => questions.filter((q) => q.id !== question.id));
+        });
+    }
+
+    function handleChangeCorrectIndex(event) {
+      const newCorrectIndex = parseInt(event.target.value);
+      setCorrectIdx(newCorrectIndex);
+
+      fetch(`http://localhost:4000/questions/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          correctIndex: newCorrectIndex
+        }),
+      })
+      .then((res) => res.json())
+      .then((updatedQuestion) => {
+        onUpdateQuestion((questions) =>
+        questions.map((q) => (q.id === id ? updatedQuestion : q))
+        );
+      });
+    }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={correctIdx} onChange={handleChangeCorrectIndex}>
+          {options}
+          </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
